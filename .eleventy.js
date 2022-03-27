47f0da8bb925862320563217f899f8c7c104450a
Eduardo Crivellari xdux <xduxdg@gmail.com>
2022-03-26 23:18:41 -0300

    fixed-syntax-highlight

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = function(eleventyConfig){
 
     eleventyConfig.addPassthroughCopy('./src/style.css');
+    eleventyConfig.addPassthroughCopy('./src/dracula.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/admin');
 
