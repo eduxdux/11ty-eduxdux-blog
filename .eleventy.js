4738e5e4584c603c13c15df354069f6e8adedb6f
Eduxdux <xduxdg@gmail.com>
2023-04-11 14:21:37 -0300

    More changes

--- .eleventy.js
+++ .eleventy.js
@@ -3,9 +3,9 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = function(eleventyConfig){
 
     eleventyConfig.addPassthroughCopy('./src/style.css');
-    eleventyConfig.addPassthroughCopy('./src/dracula.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/admin');
+    eleventyConfig.addPassthroughCopy({ "img/favicon": "/" });
 
     return{
         dir: {
